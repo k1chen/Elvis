46b028920d37d9f4692ff2a6a7e3addefb6e254c
k1chen <chenaun@bc.edu>
2025-01-20 17:31:18 -0500

    Elvis Costello Challenge

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            Spacer()
+            Image(systemName: imageName )
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+                .frame(width: 300, height: 300)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            Spacer()
+            HStack{
+                Button("Peace") {
+                    imageName = "peacesign"
+                    message = "Peace"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.purple)
+                Button("Love"){
+                    imageName = "heart"
+                    message = "Love"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.purple)
+                Button("Understanding"){
+                    imageName = "lightbulb"
+                    message = "Understanding"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.purple)
+            }
+  
         }
         .padding()
+          
     }
 }
 
