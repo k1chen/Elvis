f098e74377869c28512f723e155a3384bad6ce80
k1chen <chenaun@bc.edu>
2025-01-20 17:21:31 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Kenneth Chen on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
